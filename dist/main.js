203b50deb56abaa20fc113677aee6a665416cb6d
Alexander Marggraf <a.marggraf@stud.unibas.ch>
2025-06-29 18:20:01 +0200

    Add proper directory structure with dist folder

--- dist/main.js
+++ dist/main.js
@@ -40,7 +40,7 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
+            preload: path.join(__dirname, '../../dist/preload.js'),
         },
     });
     win.loadFile('index.html');
