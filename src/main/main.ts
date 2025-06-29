dc5cbdbbff924f803ccc8f253dda37a71d6a7f3b
Istref Uka <istref.uka55@gmail.com>
2025-06-29 17:46:42 +0200

    add testprogram for electron and typescript

---
+++ src/main/main.ts
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
