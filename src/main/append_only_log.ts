f4925aa5e18bc99affdee0a355d427269a52110f
Pascal von Fellenberg <pvonfellenberg@bluewin.ch>
2025-07-04 18:48:19 +0200

    add initial (partial) implementation of append-only log

---
+++ src/main/append_only_log.ts
@@ -0,0 +1,202 @@
+//import {v4 as uuidv4} from 'uuid';
+
+// the following functions shall be used to serialize / deserialize the append-only log
+// source: https://stackoverflow.com/a/56150320/13166601
+function replacer(key: any, value: any) {
+  if(value instanceof Map) {
+    return {
+      dataType: 'Map',
+      value: Array.from(value.entries()), // or with spread: value: [...value]
+    };
+  } else {
+    return value;
+  }
+}
+function reviver(key: any, value: any) {
+  if(typeof value === 'object' && value !== null) {
+    if (value.dataType === 'Map') {
+      return new Map(value.value);
+    }
+  }
+  return value;
+}
+// ============
+
+export type uuid = string; //`${string}-${string}-${string}-${string}-${string}`;
+
+export type Person = {
+  displayName: string,
+  uuid: uuid,
+}
+export type Operation = {
+  command: string,
+  args: string[],
+}
+
+export class LogEntry {
+  creator: uuid;
+  id: uuid;
+  operation: Operation;
+  index: number;
+  dependencies: uuid[]; // ids of log-entries
+
+  constructor(creator: uuid, id: uuid, operation: Operation, dependencies: uuid[], index: number){
+    this.creator = creator;
+    this.id = id;
+    this.operation = operation;
+    this.index = index;
+    this.dependencies = dependencies;
+  }
+}
+
+export type Frontier = Map<uuid, number>;
+
+export class AppendOnlyLog {
+  public entryMap = new Map<uuid, LogEntry[]>();
+  add_operation(creator: uuid, operation: Operation, dependencies: uuid[], entryID: uuid) {
+    if (this._search_entries(entryID) != null) {
+      throw new Error("Entry with same UUID already in AppendOnlyLog: " + entryID);
+    }
+    if (!this._has_dependencies(dependencies)) {
+      throw new Error("Couldn't find all of the following dependencies in AOL; dependencies: " + dependencies + " AOL: " + JSON.stringify(this, replacer, 2));
+    }
+    if (!this.entryMap.has(creator)) {
+      this.entryMap.set(creator, new Array<LogEntry>());
+    }
+    let curr = this.entryMap.get(creator);
+    let curr_len = curr!.length;
+    let new_entry = new LogEntry(creator, entryID, operation, dependencies, curr_len);
+    curr!.push(new_entry);
+  }
+
+  update(entries: Array<LogEntry>) {
+    for (let entry of entries) {
+      if (!this._has_dependencies(entry.dependencies)) {
+        throw new Error("Couldn't find all of the following dependencies in AOL; dependencies: " + entry.dependencies + " AOL: " + JSON.stringify(this, replacer, 2));
+      }
+      if (!this.entryMap.has(entry.creator)) {
+        this.entryMap.set(entry.creator, new Array<LogEntry>());
+      }
+      let curr = this.entryMap.get(entry.creator);
+      let curr_len = curr!.length;
+      if (curr_len !== entry.index) {
+        throw new Error("Tried to add entry that doesn't match the index");
+      }
+
+      curr!.push(entry);
+    }
+  }
+
+  get_frontier(): Frontier {
+    let res = new Map<uuid, number>();
+    for (let creator of this.entryMap.keys()) {
+      res.set(creator, this.entryMap.get(creator)!.length);
+    }
+    return res;
+  }
+
+  // 
+  /**
+   * the resulting array must be topologically sorted.
+   * algorithm used: https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+   * @param frontier other frontier to diff against our frontier
+   * @returns the LogEntries that the other replica is missing based on their frontier. In a topological order. 
+   */
+  _query_missing_ids(frontier: Frontier): Array<uuid> {
+    let all_nodes_in_subgraph = this._get_missing_ids_unsorted(frontier)
+    let subgraph = new Set<uuid>(all_nodes_in_subgraph)
+    let unmarked = new Set<uuid>(all_nodes_in_subgraph);
+    let temp_marked = new Set<uuid>();
+    let resIDs = new Array<uuid>();
+    // this function marks the nodes (log-entries) in a depth first manner
+    let visit = (nodeID: uuid) => {
+      // base case
+      if (!unmarked.has(nodeID)) return;
+
+      let current_node = this._search_entries(nodeID); 
+
+      if (temp_marked.has(nodeID)) throw new Error("This graph has a cycle!!");
+      if (current_node == null) throw new Error("node " + nodeID + " is not in graph");
+
+      let dependencies = new Set(current_node.dependencies);
+      if (current_node.index > 0) {
+        // the implicitly defined dependency to the previous entry log of the same person is added
+        dependencies.add(this.entryMap.get(current_node.creator)!.at(current_node.index - 1)!.id);
+      }
+      dependencies = dependencies.intersection(subgraph)
+      temp_marked.add(nodeID);
+
+      for (let curr of [...dependencies]) {
+        visit(curr);
+      }
+
+      temp_marked.delete(nodeID);
+      if (!unmarked.delete(nodeID)) {
+        throw new Error("could not mark node with nodeID " + nodeID);
+      }
+      resIDs.unshift(nodeID);
+    };
+
+    while (unmarked.size !== 0) {
+      let nextNode = unmarked.values().next().value!;
+      visit(nextNode);
+    }
+
+    return resIDs.reverse();
+  }
+
+  query_missing(frontier: Frontier): Array<LogEntry> {
+    let resIDs = this._query_missing_ids(frontier);
+    let res = new Array<LogEntry>();
+    for (let id of resIDs) {
+      let curr = this._search_entries(id);
+      console.assert(curr != null);
+      res.push(curr!);
+    }
+    return res;
+  }
+
+  // TODO implement query_missing_ops
+  // TODO implement save_to, load_from
+
+  _search_entries(id: uuid, creator?: uuid): LogEntry | null {
+    if (creator == null) {
+      for (let creator of this.entryMap.keys()) {
+        for (let entry of this.entryMap.get(creator)!) {
+          if (id === entry.id){
+            return entry;
+          }
+        }
+      }
+      return null;
+    }
+    if (!this.entryMap.has(creator)) {
+      Error("creator with id " + creator + " could not be found");
+    }
+    for (let entry of this.entryMap.get(creator)!) {
+      if (entry.id === id) {
+        return entry;
+      }
+    }
+    return null;
+  }
+
+  _get_missing_ids_unsorted(frontier?: Frontier): Array<uuid> {
+    if (frontier == null) {
+      frontier = new Map<uuid, number>();
+    }
+    let res = new Array<uuid>();
+    for (let creator of this.entryMap.keys()) {
+      let min_val = frontier.get(creator) ?? 0;
+      for (let entry of this.entryMap.get(creator)!.slice(min_val)) {
+        res.push(entry.id);
+      }
+    }
+    return res;
+  }
+
+  _has_dependencies(dependencies: uuid[]) {
+    return dependencies.every((val) => this._search_entries(val) != null, this);
+  }
+
+}
---
+++ src/tests/append_only_log.test.ts
@@ -0,0 +1,274 @@
+import { randomUUID } from "crypto";
+import { AppendOnlyLog, LogEntry} from "../main/append_only_log";
+
+describe("AppendOnlyLog Tests", () => {
+  it("AppendOnlyLog test _search_entries single user", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let op1 = {command: "cmd1", args: ["arg1", "arg2"]};
+    log.add_operation(creator1, op1, [], entry1);
+    log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry1], entry2);
+    let expected_search_res: LogEntry = {creator: creator1, id: entry1, operation: op1, dependencies: [], index: 0}
+    expect(log._search_entries(entry1)).toEqual(expected_search_res);
+  });
+  it("AppendOnlyLog test _search_entries multiple users", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let entry3 = randomUUID();
+    let entry4 = randomUUID();
+    let op = {command: "cmd2", args: ["arg3", "arg4", "arg5"]};
+    log.add_operation(creator1, {command: "cmd1", args: []}, [], entry1);
+    log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry1], entry2);
+    log.add_operation(creator2, {command: "cmd3", args: ["arg5"]}, [entry2, entry1], entry3);
+    log.add_operation(creator2, op, [entry1], entry4);
+    let expected_search_res: LogEntry = {creator: creator2, id: entry4, operation: op, dependencies: [entry1], index: 1};
+    expect(log._search_entries(entry4)).toEqual(expected_search_res);
+  });
+  it("AppendOnlyLog test _search_entries multiple users but one as arg", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let entry3 = randomUUID();
+    let entry4 = randomUUID();
+    let op = {command: "cmd2", args: ["arg3", "arg4", "arg5"]};
+    log.add_operation(creator1, {command: "cmd1", args: []}, [], entry1);
+    log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry1], entry2);
+    log.add_operation(creator2, {command: "cmd3", args: ["arg5"]}, [entry2, entry1], entry3);
+    log.add_operation(creator2, op, [entry1], entry4);
+    let expected_search_res: LogEntry = {creator: creator2, id: entry4, operation: op, dependencies: [entry1], index: 1};
+    expect(log._search_entries(entry4, creator2)).toEqual(expected_search_res);
+    expect(log._search_entries(entry4, creator1)).toBeNull();
+  });
+  it("AppendOnlyLog throw when same id added", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let entry1 = randomUUID();
+    let op = {command: "cmd2", args: ["arg3", "arg4", "arg5"]};
+    log.add_operation(creator1, {command: "cmd1", args: []}, [], entry1);
+    expect(() => {
+      log.add_operation(creator1, {command: "cmd1", args: []}, [], entry1);
+    }).toThrow();
+  });
+  it("AppendOnlyLog throw when adding with missing dependencies 1", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let op1 = {command: "cmd1", args: ["arg1", "arg2"]};
+    log.add_operation(creator1, op1, [], entry1);
+    expect(() => {
+      log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry2, entry1], entry2);
+    }).toThrow();
+  });
+  it("AppendOnlyLog throw when adding with missing dependencies 2", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let entry3 = randomUUID();
+    let entry_not_in_aol = randomUUID();
+    let op1 = {command: "cmd1", args: ["arg1", "arg2"]};
+    log.add_operation(creator1, op1, [], entry1);
+    log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry1], entry2);
+    expect(() => {
+      log.add_operation(creator1, {command: "cmd3", args: ["arg6"]}, [entry_not_in_aol, entry2], entry3)
+    }).toThrow();
+  });
+  it("AppendOnlyLog test update", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let entry3 = randomUUID();
+    let entry4 = randomUUID();
+    let op1 = {command: "cmd1", args: ["arg1", "arg2"]};
+    log.add_operation(creator1, op1, [], entry1);
+    log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry1], entry2);
+    let entry3obj = new LogEntry(creator1, entry3, {command: "cmd3", args: ["arg6", "arg7"]}, [entry2], 2);
+    let entry4obj = new LogEntry(creator2, entry4, {command: "cmd4", args: ["arg8"]}, [entry3], 0);
+    expect(() => {
+      log.update(new Array( entry3obj, entry4obj));
+     }).not.toThrow();
+    expect(log._search_entries(entry3)).toEqual(entry3obj);
+    expect(log._search_entries(entry4)).toEqual(entry4obj);
+  });
+  it("AppendOnlyLog test update expect fail because missing entries", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let entry3 = randomUUID();
+    let entry4 = randomUUID();
+    let op1 = {command: "cmd1", args: ["arg1", "arg2"]};
+    log.add_operation(creator1, op1, [], entry1);
+    log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry1], entry2);
+    let entry3obj = new LogEntry(creator1, entry3, {command: "cmd3", args: ["arg6", "arg7"]}, [entry2], 2);
+    let entry4obj = new LogEntry(creator2, entry4, {command: "cmd4", args: ["arg8"]}, [entry3], 1);
+    expect(() => {
+      log.update(new Array( entry3obj, entry4obj));
+     }).toThrow();
+    expect(log._search_entries(entry3)).toEqual(entry3obj);
+    expect(log._search_entries(entry4)).toBeNull();
+  });
+  it("AppendOnlyLog test get_frontier", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let creator3 = randomUUID();
+    let entry1 = randomUUID();
+    let entry2 = randomUUID();
+    let entry3 = randomUUID();
+    let entry4 = randomUUID();
+    let entry5 = randomUUID();
+    let op1 = {command: "cmd1", args: ["arg1", "arg2"]};
+    log.add_operation(creator1, op1, [], entry1);
+    log.add_operation(creator1, {command: "cmd2", args: ["arg3", "arg4", "arg5"]}, [entry1], entry2);
+    let entry3obj = new LogEntry(creator1, entry3, {command: "cmd3", args: ["arg6", "arg7"]}, [entry2], 2);
+    let entry4obj = new LogEntry(creator2, entry4, {command: "cmd4", args: ["arg8"]}, [entry3], 0);
+    log.update(new Array( entry3obj, entry4obj));
+    log.add_operation(creator3, {command: "cmd5", args: ["arg9"]}, [], entry5);
+    expect(log.get_frontier()).toEqual(new Map([
+      [creator1, 3],
+      [creator2, 1],
+      [creator3, 1],
+    ]));
+  });
+  it("AppendOnlyLog test _query_missing_ids 1", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let creator3 = randomUUID();
+    let entry0 = "entry0";
+    let entry1 = "entry1";
+    let entry2 = "entry2";
+    let entry3 = "entry3";
+    let entry4 = "entry4";
+    let entry5 = "entry5";
+    let op = {command: "", args: []};
+    log.add_operation(creator1, op, [], entry0);
+    log.add_operation(creator1, op, [entry0], entry1);
+    log.add_operation(creator1, op, [entry1], entry2);
+    log.add_operation(creator2, op, [entry0, entry2], entry3);
+    log.add_operation(creator3, op, [], entry4);
+    log.add_operation(creator1, op, [entry0, entry1, entry3, entry4], entry5);
+    let query_res = log._query_missing_ids(new Map());
+    let idx = [entry0, entry1, entry2, entry3, entry4, entry5].map((value) => {return query_res.indexOf(value)});
+    expect(query_res.length).toEqual(6);
+    expect(idx[2]).toBeGreaterThan(idx[1]);
+    expect(idx[3]).toBeGreaterThan(idx[2]);
+    expect(idx[3]).toBeGreaterThan(idx[0]);
+    expect(idx[5]).toBeGreaterThan(idx[4]);
+    expect(idx[5]).toBeGreaterThan(idx[3]);
+  });
+  it("AppendOnlyLog test _query_missing_ids 2", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let creator3 = randomUUID();
+    let entry0 = "entry0";
+    let entry1 = "entry1";
+    let entry2 = "entry2";
+    let entry3 = "entry3";
+    let entry4 = "entry4";
+    let entry5 = "entry5";
+    let op = {command: "", args: []};
+    log.add_operation(creator1, op, [], entry0);
+    log.add_operation(creator2, op, [entry0], entry1);
+    log.add_operation(creator3, op, [entry0], entry2);
+    log.add_operation(creator1, op, [entry2, entry1], entry3);
+    log.add_operation(creator1, op, [], entry4);
+    log.add_operation(creator3, op, [entry4], entry5);
+    let query_res = log._query_missing_ids(new Map());
+    let idx = [entry0, entry1, entry2, entry3, entry4, entry5].map((value) => {return query_res.indexOf(value)});
+    expect(query_res.length).toEqual(6);
+    expect(idx.every((val) => val >= 0)).toBeTruthy();
+    expect(idx[5]).toBeGreaterThan(idx[4]);
+    expect(idx[3]).toBeGreaterThan(idx[2]);
+    expect(idx[4]).toBeGreaterThan(idx[3]); // from implicit ordering within a list from a creator
+    expect(idx[1]).toBeGreaterThan(idx[0]);
+    expect(idx[2]).toBeGreaterThan(idx[0]);
+  });
+  it("AppendOnlyLog test _query_missing_ids 3", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let creator3 = randomUUID();
+    let entry0 = "entry0";
+    let entry1 = "entry1";
+    let entry2 = "entry2";
+    let entry3 = "entry3";
+    let entry4 = "entry4";
+    let entry5 = "entry5";
+    let op = {command: "", args: []};
+    log.add_operation(creator1, op, [], entry0);
+    log.add_operation(creator2, op, [entry0], entry1);
+    log.add_operation(creator3, op, [entry0], entry2);
+    log.add_operation(creator1, op, [entry2, entry1], entry3);
+    log.add_operation(creator1, op, [], entry4);
+    log.add_operation(creator3, op, [entry4], entry5);
+    let query_res = log._query_missing_ids(new Map([
+      [creator1, 2],
+      [creator2, 1],
+    ]));
+    expect(query_res.length).toEqual(3);
+  });
+  it("AppendOnlyLog test _query_missing_ids 4", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let creator3 = randomUUID();
+    let entry0 = "entry0";
+    let entry1 = "entry1";
+    let entry2 = "entry2";
+    let entry3 = "entry3";
+    let entry4 = "entry4";
+    let entry5 = "entry5";
+    let op = {command: "", args: []};
+    log.add_operation(creator1, op, [], entry0);
+    log.add_operation(creator2, op, [entry0], entry1);
+    log.add_operation(creator3, op, [entry0], entry2);
+    log.add_operation(creator1, op, [entry2, entry1], entry3);
+    log.add_operation(creator1, op, [], entry4);
+    log.add_operation(creator3, op, [entry4], entry5);
+    let query_res = log._query_missing_ids(new Map([
+      [creator1, 10],
+      [creator2, 1],
+    ]));
+    expect(query_res.length).toEqual(2);
+  });
+  it("AppendOnlyLog test _query_missing_ids 4", () => {
+    let log = new AppendOnlyLog();
+    let creator1 = randomUUID();
+    let creator2 = randomUUID();
+    let creator3 = randomUUID();
+    let entry0 = "entry0";
+    let entry1 = "entry1";
+    let entry2 = "entry2";
+    let entry3 = "entry3";
+    let entry4 = "entry4";
+    let entry5 = "entry5";
+    let op = {command: "", args: []};
+    log.add_operation(creator1, op, [], entry0);
+    log.add_operation(creator2, op, [entry0], entry1);
+    log.add_operation(creator3, op, [entry0], entry2);
+    log.add_operation(creator1, op, [entry2, entry1], entry3);
+    log.add_operation(creator1, op, [], entry4);
+    log.add_operation(creator3, op, [entry4], entry5);
+    let query_res = log._query_missing_ids(new Map([
+      [creator1, 10],
+      [creator2, 1],
+    ]));
+    expect(query_res.length).toEqual(2);
+  });
+  // TODO test query_missing, query_missing_ops
+  // TODO test save_to, load_from
+})
